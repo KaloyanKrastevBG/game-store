43c42e3e5ea1b8d04d51eb0baa1a28463eb1b1f8
KaloyanKrastevBG <50043391+KaloyanKrastevBG@users.noreply.github.com>
2020-03-13 22:11:24 +0200

    Commit init

---
+++ src/main/java/com/softuni/gamestore/controllers/AppController.java
@@ -0,0 +1,14 @@
+package com.softuni.gamestore.controllers;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AppController implements CommandLineRunner {
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
